14eb40e09f638b087434f08485f11aef28d7d95c
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-15 17:30:19 +0100

    Domain model and Neo4jQueryStore spike

--- src/EntryPoints/MediaWikiHooks.php
+++ src/EntryPoints/MediaWikiHooks.php
@@ -5,12 +5,11 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\NeoWiki\EntryPoints;
 
 use CommentStoreComment;
-use Laudis\Neo4j\Contracts\TransactionInterface;
 use MediaWiki\Revision\RenderedRevision;
 use MediaWiki\User\UserIdentity;
+use ProfessionalWiki\NeoWiki\NeoWikiExtension;
 use Status;
 use Title;
-use Laudis\Neo4j\ClientBuilder;
 
 class MediaWikiHooks {
 
@@ -27,30 +26,7 @@ class MediaWikiHooks {
 		$flags,
 		Status $hookStatus
 	): void {
-		foreach ( $renderedRevision->getRevision()->getSlots()->getSlots() as $slot ) {
-			$content = $slot->getContent();
-
-			if ( $content instanceof SubjectContent ) {
-
-				$client = ClientBuilder::create() // TODO: inject
-					->withDriver('bolt', 'bolt://neo4j:' . $_ENV['NEO4J_PASSWORD'] . '@neo:7687')
-					->withDefaultDriver('bolt')
-					->build();
-
-				$properties = (array)$content->getData()->getValue();
-				$properties['name'] = $renderedRevision->getRevision()->getPage()->getDBkey(); // TODO: real title
-
-				$client->writeTransaction( static function ( TransactionInterface $tsx ) use ( $properties ) {
-					$tsx->run(
-						<<<'CYPHER'
-CREATE ($props)
-CYPHER,
-
-						[ 'props' => $properties ] // TODO: verify security
-					);
-				} );
-			}
-		}
+		NeoWikiExtension::getInstance()->getStoreContentUC()->storeContent( $renderedRevision, $user );
 	}
 
 	public static function onCodeEditorGetPageLanguage( Title $title, ?string &$lang, ?string $model, ?string $format ): void {
