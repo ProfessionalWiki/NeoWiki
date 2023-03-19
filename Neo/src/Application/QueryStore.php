22cf67f06d7c06ae8d2592b94877f89633e16c56
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-19 23:41:43 +0100

    Allow Cypher queries from wiki pages

--- Neo/src/Application/QueryStore.php
+++ Neo/src/Application/QueryStore.php
@@ -4,10 +4,13 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\Application;
 
+use Laudis\Neo4j\Databags\SummarizedResult;
 use ProfessionalWiki\NeoWiki\Domain\Subject;
 
 interface QueryStore {
 
 	public function saveSubject( Subject $subject ): void;
-	
+
+	public function runReadQuery( string $cypher ): SummarizedResult;
+
 }
