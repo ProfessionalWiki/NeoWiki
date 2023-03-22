2204dde0846c40c51b6fa3c7d3a16eee2a5ee9c3
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-22 19:59:29 +0100

    Support multiple subjects per page and add labels
    Fixes https://github.com/ProfessionalWiki/NeoWiki/issues/3
    Fixes https://github.com/ProfessionalWiki/NeoWiki/issues/4

--- src/EntryPoints/SubjectContent.php
+++ src/EntryPoints/SubjectContent.php
@@ -4,18 +4,9 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\EntryPoints;
 
-use ProfessionalWiki\NeoWiki\Domain\Relation;
-use ProfessionalWiki\NeoWiki\Domain\RelationId;
-use ProfessionalWiki\NeoWiki\Domain\RelationList;
-use ProfessionalWiki\NeoWiki\Domain\RelationProperties;
-use ProfessionalWiki\NeoWiki\Domain\RelationTypeId;
-use ProfessionalWiki\NeoWiki\Domain\Subject;
-use ProfessionalWiki\NeoWiki\Domain\SubjectId;
-use ProfessionalWiki\NeoWiki\Domain\SubjectProperties;
-use ProfessionalWiki\NeoWiki\Domain\SubjectTypeId;
-use ProfessionalWiki\NeoWiki\Domain\SubjectTypeIdList;
-use Status;
-use stdClass;
+use FormatJson;
+use ProfessionalWiki\NeoWiki\Domain\SubjectMap;
+use ProfessionalWiki\NeoWiki\Persistence\SubjectSlotDeserializer;
 
 class SubjectContent extends \JsonContent {
 
@@ -28,59 +19,12 @@ class SubjectContent extends \JsonContent {
 		);
 	}
 
-	public function getData(): Status {
-		$status = parent::getData();
-
-		if ( $status->isGood() && !$this->isSubjectJson( $status->getValue() ) ) {
-			return Status::newFatal( 'Invalid NeoWiki Subject' );
-		}
-
-		return $status;
+	public function beautifyJSON(): string {
+		return FormatJson::encode( json_decode( $this->getText() ), true, FormatJson::UTF8_OK );
 	}
 
-	private function isSubjectJson( stdClass $json ): bool {
-		return true; // TODO: validate format
-	}
-
-	public function getSubject(): Subject {
-		$jsonArray = (array)$this->getData()->getValue();
-
-		return new Subject(
-			id: new SubjectId( $jsonArray['id'] ),
-			types: $this->newSubjectTypeIdList( $jsonArray ),
-			relations: $this->newRelationList( $jsonArray ),
-			properties: $this->newSubjectProperties( $jsonArray ),
-		);
-	}
-
-	private function newSubjectTypeIdList( array $jsonArray ): SubjectTypeIdList {
-		return new SubjectTypeIdList(
-			array_map(
-				fn( string $id ) => new SubjectTypeId( $id ),
-				(array)( $jsonArray['types'] ?? [] )
-			)
-		);
-	}
-
-	private function newRelationList( array $jsonArray ): RelationList {
-		$relations = (array)( $jsonArray['relations'] ?? [] );
-
-		return new RelationList(
-			array_map(
-				fn( string $id, stdClass $relation ) => new Relation(
-					id: new RelationId( $id ),
-					type: new RelationTypeId( $relation->type ),
-					targetId: new SubjectId( $relation->target ),
-					properties: new RelationProperties( (array)( ( (array)$relation )['properties'] ?? [] ) ),
-				),
-				array_keys( $relations ),
-				$relations
-			)
-		);
-	}
-
-	private function newSubjectProperties( array $jsonArray ): SubjectProperties {
-		return new SubjectProperties( (array)( $jsonArray['properties'] ?? [] ) );
+	public function getSubjects(): SubjectMap {
+		return ( new SubjectSlotDeserializer() )->deserialize( $this->getText() );
 	}
 
 }
