a1d21d53983600fe5381e4b0e73014cd049d9a01
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-25 00:15:46 +0100

    Add basic table output to #cypher

--- src/EntryPoints/CypherFunction.php
+++ src/EntryPoints/CypherFunction.php
@@ -5,20 +5,28 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\NeoWiki\EntryPoints;
 
 use Laudis\Neo4j\Types\CypherList;
+use Laudis\Neo4j\Types\Node;
 use Parser;
 use ProfessionalWiki\NeoWiki\Application\QueryStore;
+use ProfessionalWiki\NeoWiki\Presentation\TemplateRenderer;
 
 class CypherFunction {
 
 	public function __construct(
-		private readonly QueryStore $queryStore
+		private readonly QueryStore $queryStore,
+		private readonly TemplateRenderer $templateRenderer,
 	) {
 	}
 
+	/**
+	 * @return array<mixed, mixed>
+	 */
 	public function handleParserFunctionCall( Parser $parser, string ...$arguments ): array {
+		// TODO: check if argument exists
+
 		$results = $this->queryStore->runReadQuery( $arguments[0] )->getResults();
 
-		return  [
+		return [
 			$this->createMediaWikiHtmlTable( $results ) . $this->createJsonOutput( $results ),
 			'noparse' => true,
 			'isHTML' => true,
@@ -26,10 +34,65 @@ class CypherFunction {
 	}
 
 	private function createMediaWikiHtmlTable( CypherList $results ): string {
-		return \Html::element(
-			'pre',
-			[],
-			json_encode( $results->toRecursiveArray(), JSON_PRETTY_PRINT )
+		return $this->templateRenderer->viewModelToString(
+			'Table.html.twig',
+			[
+				'subjects' => $this->resultsToViewModel( $results ),
+			]
+		);
+	}
+
+	private function resultsToViewModel( CypherList $results ): array {
+		$subjects = [];
+
+		foreach ( $results->toRecursiveArray() as $result ) {
+			foreach ( $result as $varValue ) {
+				if ( $varValue instanceof Node && $this->isSubjectNode( $varValue ) ) {
+					$subjects[] = $this->subjectNodeToArray( $varValue );
+				}
+			}
+		}
+
+		return $this->defaultEmptyProperties( $subjects );
+	}
+
+	private function isSubjectNode( Node $node ): bool {
+		return $node->getProperties()->hasKey( 'id' ); // TODO: make this more robust
+	}
+
+	private function subjectNodeToArray( Node $subject ): array {
+		return [
+			'id' => $subject->getProperties()->get( 'id' ),
+			'types' => $subject->getLabels(),
+			'properties' => array_diff_key( $subject->getProperties()->toRecursiveArray(), [ 'id' => '' ] )
+		];
+	}
+
+	private function defaultEmptyProperties( array $subjects ): array {
+		$propertyNames = $this->getAllPropertyNames( $subjects );
+
+		return array_map(
+			function( array $subject ) use ( $propertyNames ): array {
+				$subject['properties'] = array_merge(
+					array_fill_keys( $propertyNames, '' ),
+					$subject['properties']
+				);
+
+				return $subject;
+			},
+			$subjects
+		);
+	}
+
+	private function getAllPropertyNames( array $subjects ): array {
+		return array_unique(
+			array_reduce(
+				$subjects,
+				function( array $carry, array $subject ): array {
+					return array_merge( $carry, array_keys( $subject['properties'] ) );
+				},
+				[]
+			)
 		);
 	}
 
