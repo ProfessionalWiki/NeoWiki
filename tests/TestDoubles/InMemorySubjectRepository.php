4b3ac506037084f64e8141086a0b8028dd69d719
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-25 23:41:26 +0100

    Add REST API subject patch stub

---
+++ tests/TestDoubles/InMemorySubjectRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\Tests\TestDoubles;
+
+use ProfessionalWiki\NeoWiki\Application\SubjectRepository;
+use ProfessionalWiki\NeoWiki\Domain\Subject;
+use ProfessionalWiki\NeoWiki\Domain\SubjectId;
+
+class InMemorySubjectRepository implements SubjectRepository {
+
+	/**
+	 * @var array<string, Subject> Subjects indexed by their ID
+	 */
+	private array $subjects = [];
+
+	public function getSubject( SubjectId $subjectId ): ?Subject {
+		return $this->subjects[$subjectId->text] ?? null;
+	}
+
+	public function storeSubject( Subject $subject ): void {
+		$this->subjects[$subject->id->text] = $subject;
+	}
+
+}
