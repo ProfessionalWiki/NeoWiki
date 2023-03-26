e522c14f9e5608465fe2c9d57627fa04051c928d
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-26 23:39:08 +0200

    Add MediaWikiSubjectRepository

---
+++ tests/TestDoubles/InMemoryQueryStore.php
@@ -0,0 +1,42 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\Tests\TestDoubles;
+
+use Laudis\Neo4j\Databags\SummarizedResult;
+use ProfessionalWiki\NeoWiki\Application\QueryStore;
+use ProfessionalWiki\NeoWiki\Domain\SubjectId;
+use ProfessionalWiki\NeoWiki\Domain\SubjectMap;
+
+class InMemoryQueryStore implements QueryStore {
+
+	/**
+	 * @var array<int, SubjectMap>
+	 */
+	private array $subjectsByPageId = [];
+
+	public function savePage( int $pageId, string $pageTitle, SubjectMap $subjects ): void {
+		$this->subjectsByPageId[$pageId] = $subjects;
+	}
+
+	public function deletePage( int $pageId ): void {
+		unset( $this->subjectsByPageId[$pageId] );
+	}
+
+	public function runReadQuery( string $cypher ): SummarizedResult {
+		$summary = null;
+		return new SummarizedResult( $summary, [] );
+	}
+
+	public function getPageIdForSubject( SubjectId $subjectId ): ?int {
+		foreach ( $this->subjectsByPageId as $pageId => $subjectMap ) {
+			if ( $subjectMap->hasSubject( $subjectId ) ) {
+				return $pageId;
+			}
+		}
+
+		return null;
+	}
+
+}
