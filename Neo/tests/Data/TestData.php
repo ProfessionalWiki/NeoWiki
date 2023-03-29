a33ce9b7f66cae0ae1717f99bfde2c37bc73f71c
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-30 00:05:32 +0200

    Reorganize Persistence classes

---
+++ Neo/tests/Data/TestData.php
@@ -0,0 +1,13 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\Tests\Data;
+
+class TestData {
+
+	public static function getFileContents( string $fileName ): string {
+		return file_get_contents( __DIR__ . '/' . $fileName );
+	}
+
+}
