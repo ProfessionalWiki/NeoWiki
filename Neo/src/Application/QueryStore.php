37783c6854b12d7bf54a53522b10760e631865a0
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-03-31 20:00:20 +0200

    Add QueryEngine interface

--- Neo/src/Application/QueryStore.php
+++ Neo/src/Application/QueryStore.php
@@ -4,15 +4,16 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\Application;
 
-use Laudis\Neo4j\Databags\SummarizedResult;
 use ProfessionalWiki\NeoWiki\Domain\Page\Page;
+use ProfessionalWiki\NeoWiki\Domain\Page\PageId;
 
+/**
+ * "Secondary" or "read/query" persistence
+ */
 interface QueryStore {
 
 	public function savePage( Page $page ): void;
 
-	public function deletePage( int $pageId ): void;
-
-	public function runReadQuery( string $cypher ): SummarizedResult;
+	public function deletePage( PageId $pageId ): void;
 
 }
