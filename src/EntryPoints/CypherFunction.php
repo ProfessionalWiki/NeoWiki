6efca90be5e9b71da5676dddc054422c3b0ff955
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-04-01 16:29:19 +0200

    Extract RunCypherQuery and presenter

--- src/EntryPoints/CypherFunction.php
+++ src/EntryPoints/CypherFunction.php
@@ -4,10 +4,10 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\EntryPoints;
 
-use Laudis\Neo4j\Types\CypherList;
-use Laudis\Neo4j\Types\Node;
 use Parser;
+use ProfessionalWiki\NeoWiki\Application\Queries\RunCypher\RunCypherQuery;
 use ProfessionalWiki\NeoWiki\Application\QueryEngine;
+use ProfessionalWiki\NeoWiki\Presentation\RunCypherParserFunctionPresenter;
 use ProfessionalWiki\NeoWiki\Presentation\TemplateRenderer;
 
 class CypherFunction {
@@ -22,106 +22,16 @@ class CypherFunction {
 	 * @return array<mixed, mixed>
 	 */
 	public function handleParserFunctionCall( Parser $parser, string ...$arguments ): array {
-		$parser->getOutput()->addModules( [ 'ext.neowiki.editor' ] );
+		$presenter = new RunCypherParserFunctionPresenter( $this->templateRenderer );
 
-		if ( trim( $arguments[0] ) === '' ) {
-			return $this->buildParserFunctionHtmlResponse( 'Cypher query is missing' );
-		}
-
-		$results = $this->queryEngine->runReadQuery( $arguments[0] )->getResults();
-
-		return $this->buildParserFunctionHtmlResponse(
-			$this->createMediaWikiHtmlTable( $results ) . $this->createJsonOutput( $results )
+		$cypherQueryQuery = new RunCypherQuery(
+			$this->queryEngine,
+			$presenter
 		);
-	}
 
-	private function buildParserFunctionHtmlResponse( string $html ): array {
-		return [
-			$html,
-			'noparse' => true,
-			'isHTML' => true,
-		];
-	}
+		$cypherQueryQuery->runCypher( $arguments[0] );
 
-	private function createMediaWikiHtmlTable( CypherList $results ): string {
-		return $this->templateRenderer->viewModelToString(
-			'Table.html.twig',
-			[
-				'subjects' => $this->resultsToViewModel( $results ),
-			]
-		);
-	}
-
-	private function resultsToViewModel( CypherList $results ): array {
-		$subjects = [];
-
-		foreach ( $results->toRecursiveArray() as $result ) {
-			foreach ( $result as $varValue ) {
-				if ( $varValue instanceof Node && $this->isSubjectNode( $varValue ) ) {
-					$subjects[] = $this->subjectNodeToArray( $varValue );
-				}
-			}
-		}
-
-		return $this->defaultEmptyProperties( $subjects );
-	}
-
-	private function isSubjectNode( Node $node ): bool {
-		return $node->getProperties()->hasKey( 'id' ); // TODO: make this more robust
-	}
-
-	private function subjectNodeToArray( Node $subject ): array {
-		return [
-			'id' => $subject->getProperties()->get( 'id' ),
-			'types' => $subject->getLabels(),
-			'properties' => $this->getPropertiesFromSubjectNode( $subject )
-		];
-	}
-
-	private function getPropertiesFromSubjectNode( Node $subject ): array {
-		return array_map(
-			fn( mixed $value ): array => [ 'values' => (array)$value ],
-			array_diff_key(
-				$subject->getProperties()->toRecursiveArray(),
-				[ 'id' => '' ]
-			)
-		);
-	}
-
-	private function defaultEmptyProperties( array $subjects ): array {
-		$propertyNames = $this->getAllPropertyNames( $subjects );
-
-		return array_map(
-			function( array $subject ) use ( $propertyNames ): array {
-				$subject['properties'] = array_merge(
-					array_fill_keys( $propertyNames, [ 'values' => [] ] ),
-					$subject['properties']
-				);
-
-				return $subject;
-			},
-			$subjects
-		);
-	}
-
-	private function getAllPropertyNames( array $subjects ): array {
-		return array_unique(
-			array_reduce(
-				$subjects,
-				function( array $carry, array $subject ): array {
-					return array_merge( $carry, array_keys( $subject['properties'] ) );
-				},
-				[]
-			)
-		);
-	}
-
-	private function createJsonOutput( CypherList $results ): string {
-		return \Html::element(
-			'pre',
-			[],
-			json_encode( $results->toRecursiveArray(), JSON_PRETTY_PRINT )
-		);
+		return $presenter->getParserFunctionResponseStructure();
 	}
 
 }
