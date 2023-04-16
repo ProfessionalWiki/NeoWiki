9ff55b119d4b8aaea0f3ca2030734c43f1d7ce2a
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-04-16 21:48:21 +0200

    Add Schema NS

---
+++ src/EntryPoints/Content/SchemaContentHandler.php
@@ -0,0 +1,17 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\EntryPoints\Content;
+
+class SchemaContentHandler extends \JsonContentHandler {
+
+	protected function getContentClass(): string {
+		return SchemaContent::class;
+	}
+
+	public function makeEmptyContent(): SchemaContent {
+		return new SchemaContent( '{}' );
+	}
+
+}
