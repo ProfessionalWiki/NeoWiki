3b3a8bc289c2937c24d40154eae92d2d83dfa147
Morne Alberts <morne@malberts.net>
2023-04-19 17:51:08 +0200

    Add dirty POC for creating pages and subjects from schema

---
+++ src/Presentation/FormCreateSubjectPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\Presentation;
+
+use ProfessionalWiki\NeoWiki\Application\Actions\CreateSubject\CreateSubjectPresenter;
+
+class FormCreateSubjectPresenter implements CreateSubjectPresenter {
+
+	public function presentCreated( string $subjectId ): void {
+		// TODO: Implement presentCreated() method.
+	}
+
+	public function presentInvalidRequest(): void {
+		// TODO: Implement presentInvalidRequest() method.
+	}
+
+}
