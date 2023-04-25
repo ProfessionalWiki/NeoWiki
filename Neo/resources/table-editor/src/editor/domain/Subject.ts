2dbc6ada5da0e7cd692ed7242be7b0d3a48af042
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-04-25 23:01:43 +0200

    Use subject data in auto infobox

--- Neo/resources/table-editor/src/editor/domain/Subject.ts
+++ Neo/resources/table-editor/src/editor/domain/Subject.ts
@@ -3,6 +3,7 @@ import type { SubjectId } from '@/editor/domain/SubjectId';
 export type SubjectProperties = Record<string, any>;
 
 export class Subject {
+
 	public constructor(
 		private readonly id: SubjectId,
 		private readonly label: string,
@@ -10,4 +11,21 @@ export class Subject {
 		private properties: SubjectProperties
 	) {
 	}
+
+	public getId(): SubjectId {
+		return this.id;
+	}
+
+	public getLabel(): string {
+		return this.label;
+	}
+
+	public getTypes(): string[] {
+		return this.types;
+	}
+
+	public getProperties(): SubjectProperties {
+		return this.properties;
+	}
+
 }
