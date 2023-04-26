f2f91e40fe0ad21e9b463cff7003a7a3be253008
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-04-27 01:23:22 +0200

    Step towards formatting booleans in auto infobox

--- Neo/resources/table-editor/src/editor/domain/Schema.ts
+++ Neo/resources/table-editor/src/editor/domain/Schema.ts
@@ -10,29 +10,6 @@ export type PropertyDefinition = {
 	uniqueItems?: boolean;
 };
 
-export class Schema {
-
-	public constructor(
-		private readonly title: string,
-		private readonly description: string,
-		private readonly properties: Record<string, PropertyDefinition>
-	) {
-	}
-
-	public getTitle(): string {
-		return this.title;
-	}
-
-	public getDescription(): string {
-		return this.description;
-	}
-
-	public getPropertyDefinitions(): Record<string, PropertyDefinition> {
-		return this.properties;
-	}
-
-}
-
 export enum ValueType {
 
 	String = 'string',
@@ -62,5 +39,40 @@ export enum ValueFormat {
 
 	Checkbox = 'checkbox',
 	Toggle = 'toggle',
+
+}
+
+export class Schema {
+
+	public constructor(
+		private readonly title: string,
+		private readonly description: string,
+		private readonly properties: Record<string, PropertyDefinition>
+	) {
+	}
+
+	public getTitle(): string {
+		return this.title;
+	}
+
+	public getDescription(): string {
+		return this.description;
+	}
+
+	public getPropertyDefinitions(): Record<string, PropertyDefinition> {
+		return this.properties;
+	}
+
+	public getPropertyValueType( propertyName: string ): ValueType {
+		// FIXME: the schema format in the test and on my local wiki differ. Do we use the simple or the complex format?
+		// For the complex format we need to get at "properties.properties.properties" instead of just "properties".
+		// See employeeSchema.json for the complex format.
+
+		if ( this.properties[ propertyName ] ) {
+			return this.properties[ propertyName ].type as ValueType;
+		}
+
+		return ValueType.String; // TODO: is that what we want?
+	}
 
 }
