ed7641de8c87d61a7aad5bff87f1b6f1d95574f8
mtn-andrewp <andreip@mtnhausdigital.com>
2023-05-05 17:13:35 +0100

    code sniffer validation

--- tests/EntryPoints/SpecialPages/SpecialNeoJsonTest.php
+++ tests/EntryPoints/SpecialPages/SpecialNeoJsonTest.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace ProfessionalWiki\NeoWiki\Tests\EntryPoints\SpecialPages;
 
 use ProfessionalWiki\NeoWiki\EntryPoints\SpecialPages\SpecialNeoJson;
@@ -8,18 +7,19 @@ use SpecialPageTestBase;
 
 class SpecialNeoJsonTest extends SpecialPageTestBase
 {
-	protected function newSpecialPage(): SpecialNeoJson {
-		return new SpecialNeoJson();
-	}
+    protected function newSpecialPage(): SpecialNeoJson
+    {
+        return new SpecialNeoJson();
+    }
 
-	public function testPageExists(): void {
-		/** @var string $output */
-		[ $output ] = $this->executeSpecialPage();
+    public function testPageExists(): void
+    {
+        /** @var string $output */
+        [ $output ] = $this->executeSpecialPage();
 
-		$this->assertStringContainsString(
-			'(neojson-summary)',
-			$output
-		);
-	}
-
+        $this->assertStringContainsString(
+            '(neojson-summary)',
+            $output
+        );
+    }
 }
