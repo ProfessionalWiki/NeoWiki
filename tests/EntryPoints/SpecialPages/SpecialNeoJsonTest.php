717704d0185ac29011b20d2233eafdba11d37391
mtn-andrewp <andreip@mtnhausdigital.com>
2023-05-05 17:42:42 +0100

    wiki custom code style applying

--- tests/EntryPoints/SpecialPages/SpecialNeoJsonTest.php
+++ tests/EntryPoints/SpecialPages/SpecialNeoJsonTest.php
@@ -5,19 +5,21 @@ namespace ProfessionalWiki\NeoWiki\Tests\EntryPoints\SpecialPages;
 use ProfessionalWiki\NeoWiki\EntryPoints\SpecialPages\SpecialNeoJson;
 use SpecialPageTestBase;
 
-class SpecialNeoJsonTest extends SpecialPageTestBase
-{
-    protected function newSpecialPage(): SpecialNeoJson {
-        return new SpecialNeoJson();
-    }
+/**
+ * @covers \ProfessionalWiki\NeoWiki\EntryPoints\SpecialNeoJson
+ */
+class SpecialNeoJsonTest extends SpecialPageTestBase {
+	protected function newSpecialPage(): SpecialNeoJson {
+		return new SpecialNeoJson();
+	}
 
-    public function testPageExists(): void {
-        /** @var string $output */
-        [ $output ] = $this->executeSpecialPage();
+	public function testPageExists(): void {
+		/** @var string $output */
+		[ $output ] = $this->executeSpecialPage();
 
-        $this->assertStringContainsString(
-            '(neojson-summary)',
-            $output
-        );
-    }
+		$this->assertStringContainsString(
+			'(neojson-summary)',
+			$output
+		);
+	}
 }
