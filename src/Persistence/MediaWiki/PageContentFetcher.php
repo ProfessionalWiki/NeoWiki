6bc8e1ec73e933b47178d3ad7a5f3aaf78a6c946
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-07 01:23:32 +0200

    Partial implementation of WikiPageSchemaRepository

--- src/Persistence/MediaWiki/PageContentFetcher.php
+++ src/Persistence/MediaWiki/PageContentFetcher.php
@@ -7,6 +7,7 @@ namespace ProfessionalWiki\NeoWiki\Persistence\MediaWiki;
 use Content;
 use MalformedTitleException;
 use MediaWiki\Revision\RevisionLookup;
+use MediaWiki\Revision\RevisionRecord;
 use MediaWiki\Revision\SlotRecord;
 use TitleParser;
 
@@ -20,9 +21,9 @@ class PageContentFetcher {
 		$this->revisionLookup = $revisionLookup;
 	}
 
-	public function getPageContent( string $pageTitle ): ?Content {
+	public function getPageContent( string $pageTitle, int $defaultNamespace = NS_MAIN ): ?Content {
 		try {
-			$title = $this->titleParser->parseTitle( $pageTitle );
+			$title = $this->titleParser->parseTitle( $pageTitle, $defaultNamespace );
 		}
 		catch ( MalformedTitleException ) {
 			return null;
@@ -30,7 +31,7 @@ class PageContentFetcher {
 
 		$revision = $this->revisionLookup->getRevisionByTitle( $title );
 
-		return $revision?->getContent( SlotRecord::MAIN );
+		return $revision?->getContent( SlotRecord::MAIN ); // TODO: RevisionRecord::FOR_THIS_USER, $this->authority
 	}
 
 }
