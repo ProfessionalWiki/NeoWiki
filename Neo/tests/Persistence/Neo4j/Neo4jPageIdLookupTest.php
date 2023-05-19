48a7aa68a3539f91d1c0b7e8db5696a73849f11e
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-19 23:21:41 +0200

    Create PageIdentifiers and return title from Neo4JPageIdentifiersLookup
    For https://github.com/ProfessionalWiki/NeoWiki/issues/272

--- Neo/tests/Persistence/Neo4j/Neo4jPageIdLookupTest.php
+++ Neo/tests/Persistence/Neo4j/Neo4jPageIdLookupTest.php
@@ -5,36 +5,33 @@ declare( strict_types = 1 );
 namespace ProfessionalWiki\NeoWiki\Tests\Persistence\Neo4j;
 
 use Laudis\Neo4j\Contracts\ClientInterface;
-use PHPUnit\Framework\TestCase;
+use ProfessionalWiki\NeoWiki\Domain\Page\PageId;
+use ProfessionalWiki\NeoWiki\Domain\Page\PageIdentifiers;
+use ProfessionalWiki\NeoWiki\Domain\Page\PageProperties;
 use ProfessionalWiki\NeoWiki\Domain\Subject\SubjectId;
 use ProfessionalWiki\NeoWiki\Domain\Subject\SubjectMap;
 use ProfessionalWiki\NeoWiki\NeoWikiExtension;
-use ProfessionalWiki\NeoWiki\Persistence\Neo4j\Neo4jPageIdLookup;
+use ProfessionalWiki\NeoWiki\Persistence\Neo4j\Neo4JPageIdentifiersLookup;
 use ProfessionalWiki\NeoWiki\Persistence\Neo4j\Neo4jQueryStore;
 use ProfessionalWiki\NeoWiki\Tests\Data\TestPage;
 use ProfessionalWiki\NeoWiki\Tests\Data\TestSubject;
+use ProfessionalWiki\NeoWiki\Tests\NeoWikiIntegrationTestCase;
 
 /**
- * @covers \ProfessionalWiki\NeoWiki\Persistence\Neo4j\Neo4jPageIdLookup
+ * @covers \ProfessionalWiki\NeoWiki\Persistence\Neo4j\Neo4JPageIdentifiersLookup
  */
-class Neo4jPageIdLookupTest extends TestCase {
+class Neo4jPageIdLookupTest extends NeoWikiIntegrationTestCase {
 
 	public function setUp(): void {
-		try {
-			$client = NeoWikiExtension::getInstance()->getNeo4jClient();
-			$client->run( 'MATCH (n) DETACH DELETE n' );
-		}
-		catch ( \Exception $e ) {
-			$this->markTestSkipped( 'Neo4j not available' );
-		}
+		$this->setUpNeo4j();
 	}
 
 	public function testReturnsNullOnEmptyGraph(): void {
 		$this->assertNull( $this->newLookup()->getPageIdOfSubject( new SubjectId( '404' ) ) );
 	}
 
-	private function newLookup( ClientInterface $client = null ): Neo4jPageIdLookup {
-		return new Neo4jPageIdLookup(
+	private function newLookup( ClientInterface $client = null ): Neo4JPageIdentifiersLookup {
+		return new Neo4JPageIdentifiersLookup(
 			client: $client ?? $this->getClient()
 		);
 	}
@@ -49,6 +46,7 @@ class Neo4jPageIdLookupTest extends TestCase {
 
 		$queryStore->savePage( TestPage::build(
 			id: 1,
+			properties: new PageProperties( title: 'Foo' ),
 			childSubjects: new SubjectMap(
 				TestSubject::build( id: 'GUID-4' ),
 			)
@@ -56,6 +54,7 @@ class Neo4jPageIdLookupTest extends TestCase {
 
 		$queryStore->savePage( TestPage::build(
 			id: 42,
+			properties: new PageProperties( title: 'Bar' ),
 			childSubjects: new SubjectMap(
 				TestSubject::build( id: 'GUID-1' ),
 				TestSubject::build( id: 'GUID-2' ), // Target
@@ -65,13 +64,14 @@ class Neo4jPageIdLookupTest extends TestCase {
 
 		$queryStore->savePage( TestPage::build(
 			id: 32202,
+			properties: new PageProperties( title: 'Baz' ),
 			childSubjects: new SubjectMap(
 				TestSubject::build( id: 'GUID-5' ),
 			)
 		) );
 
-		$this->assertSame(
-			42,
+		$this->assertEquals(
+			new PageIdentifiers( new PageId( 42 ), 'Bar' ),
 			$this->newLookup( $client )->getPageIdOfSubject( new SubjectId( 'GUID-2' ) )
 		);
 	}
