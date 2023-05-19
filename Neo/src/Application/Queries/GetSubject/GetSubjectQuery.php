ce78818287bc9e3c117ba834033ac492849cabeb
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-19 23:44:41 +0200

    Add page identifiers to GetSubjectResponse

--- Neo/src/Application/Queries/GetSubject/GetSubjectQuery.php
+++ Neo/src/Application/Queries/GetSubject/GetSubjectQuery.php
@@ -4,7 +4,9 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\Application\Queries\GetSubject;
 
+use ProfessionalWiki\NeoWiki\Application\PageIdentifiersLookup;
 use ProfessionalWiki\NeoWiki\Application\SubjectLookup;
+use ProfessionalWiki\NeoWiki\Domain\Page\PageIdentifiers;
 use ProfessionalWiki\NeoWiki\Domain\Subject\Subject;
 use ProfessionalWiki\NeoWiki\Domain\Subject\SubjectId;
 
@@ -12,11 +14,12 @@ class GetSubjectQuery {
 
 	public function __construct(
 		private readonly GetSubjectPresenter $presenter,
-		private readonly SubjectLookup $subjectLookup
+		private readonly SubjectLookup $subjectLookup,
+		private readonly PageIdentifiersLookup $pageIdentifiersLookup,
 	) {
 	}
 
-	public function execute( string $subjectId ): void {
+	public function execute( string $subjectId, bool $includePageIdentifiers ): void {
 		$subject = $this->subjectLookup->getSubject( new SubjectId( $subjectId ) );
 
 		if ( $subject === null ) {
@@ -24,15 +27,20 @@ class GetSubjectQuery {
 			return;
 		}
 
-		$this->presenter->presentSubject( $this->createResponse( $subject ) );
+		$this->presenter->presentSubject( $this->createResponse(
+			subject: $subject,
+			pageIdentifiers: $includePageIdentifiers ? $this->pageIdentifiersLookup->getPageIdOfSubject( $subject->id ) : null
+		) );
 	}
 
-	private function createResponse( Subject $subject ): GetSubjectResponse {
+	private function createResponse( Subject $subject, ?PageIdentifiers $pageIdentifiers ): GetSubjectResponse {
 		return new GetSubjectResponse(
 			id: $subject->id->text,
 			label: $subject->label->text,
 			schemaId: $subject->getSchemaId()->getText(),
-			properties: $subject->getProperties()->asMap() + $subject->getRelations()->asMap()
+			properties: $subject->getProperties()->asMap() + $subject->getRelations()->asMap(),
+			pageId: $pageIdentifiers?->getId()->id,
+			pageTitle: $pageIdentifiers?->getTitle(),
 		);
 	}
 
