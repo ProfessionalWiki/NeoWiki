081142ba0c4e28c871a166b72c0310aacdc423ea
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-20 16:57:28 +0200

    Add `expand` parameter to `GetSubjectApi`

--- src/EntryPoints/REST/GetSubjectApi.php
+++ src/EntryPoints/REST/GetSubjectApi.php
@@ -12,11 +12,20 @@ use Wikimedia\ParamValidator\ParamValidator;
 
 class GetSubjectApi extends SimpleHandler {
 
+	private const EXPAND_PAGE = 'page';
+	private const EXPAND_RELATIONS = 'relations';
+
 	public function run( string $subjectId ): Response {
 		$presenter = new RestGetSubjectPresenter();
 		$query = NeoWikiExtension::getInstance()->newGetSubjectQuery( $presenter );
 
-		$query->execute( $subjectId, false, false ); // TODO: add parameters
+		$expendOptions = explode( '|', $this->getRequest()->getQueryParams()['expand'] ?? '' );
+
+		$query->execute(
+			subjectId: $subjectId,
+			includePageIdentifiers: in_array( self::EXPAND_PAGE, $expendOptions ),
+			includeReferencedSubjects: in_array( self::EXPAND_RELATIONS, $expendOptions )
+		);
 
 		return $this->getResponseFactory()->createJson( $presenter->getJsonArray() );
 	}
@@ -27,6 +36,15 @@ class GetSubjectApi extends SimpleHandler {
 				self::PARAM_SOURCE => 'path',
 				ParamValidator::PARAM_TYPE => 'string',
 				ParamValidator::PARAM_REQUIRED => true,
+			],
+			'expand' => [
+				self::PARAM_SOURCE => 'query',
+				ParamValidator::PARAM_TYPE => [
+					self::EXPAND_PAGE,
+					self::EXPAND_RELATIONS,
+				],
+				ParamValidator::PARAM_ISMULTI => true,
+				ParamValidator::PARAM_REQUIRED => false,
 			],
 		];
 	}
