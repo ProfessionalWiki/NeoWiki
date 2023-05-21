125529ea64491f5a915b560f6406dff89eb2849d
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-22 00:38:55 +0200

    Simplify newTestSubject

--- Neo/resources/table-editor/src/editor/domain/TestSubject.ts
+++ Neo/resources/table-editor/src/editor/domain/TestSubject.ts
@@ -3,17 +3,19 @@ import type { SubjectProperties } from '@/editor/domain/Subject';
 import { SubjectId } from '@/editor/domain/SubjectId';
 
 export const ZERO_GUID = '00000000-0000-0000-0000-000000000000';
+export const DEFAULT_TEST_SUBJECT_LABEL = 'Test subject';
+export const DEFAULT_TEST_SCHEMA_ID = 'TestSchema';
 
 export function newTestSubject(
-	id: string|SubjectId|null = null,
-	label: string|null = null,
-	schemaId: string|null = null,
+	id: string|SubjectId = ZERO_GUID,
+	label: string = DEFAULT_TEST_SUBJECT_LABEL,
+	schemaId: string = DEFAULT_TEST_SCHEMA_ID,
 	properties: SubjectProperties = {}
 ): Subject {
 	return new Subject(
-		id instanceof SubjectId ? id : new SubjectId( id || ZERO_GUID ),
-		label || 'Test subject',
-		schemaId || 'TestSchema',
+		id instanceof SubjectId ? id : new SubjectId( id ),
+		label,
+		schemaId,
 		properties
 	);
 }
