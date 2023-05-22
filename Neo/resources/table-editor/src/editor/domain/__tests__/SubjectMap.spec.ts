d81d802b0327c831d743eccbcde9cdb6eadd46fb
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-23 00:53:09 +0200

    Show relation label and real hovercard values
    Mostly solves
    * https://github.com/ProfessionalWiki/NeoWiki/issues/249 (still need to link the page properly)
    * https://github.com/ProfessionalWiki/NeoWiki/issues/293 (need hovercard per value)

--- Neo/resources/table-editor/src/editor/domain/__tests__/SubjectMap.spec.ts
+++ Neo/resources/table-editor/src/editor/domain/__tests__/SubjectMap.spec.ts
@@ -17,8 +17,8 @@ describe( 'SubjectMap', () => {
 	);
 
 	it( 'should add elements in constructor', () => {
-		expect( subjects.get( new SubjectId( '00000000-0000-0000-0000-000000000000' ) )?.getLabel() ).toBe( 'John Doe' );
 		expect( subjects.get( new SubjectId( '00000000-0000-0000-0000-000000000001' ) )?.getLabel() ).toBe( 'Foo Bar' );
+		expect( subjects.get( new SubjectId( '00000000-0000-0000-0000-000000000000' ) )?.getLabel() ).toBe( 'John Doe' );
 	} );
 
 	it( 'should return undefined when getting by unknown ID', () => {
