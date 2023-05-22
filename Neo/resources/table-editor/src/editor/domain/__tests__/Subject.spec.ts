a09ed74b9400ebbf902798029589837a3b0a9e41
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-22 23:07:01 +0200

    Support non-array references in getIdsOfReferencedSubjects

--- Neo/resources/table-editor/src/editor/domain/__tests__/Subject.spec.ts
+++ Neo/resources/table-editor/src/editor/domain/__tests__/Subject.spec.ts
@@ -41,7 +41,7 @@ describe( 'Subject', () => {
 			const subject = newTestSubject( {
 				properties: {
 					Property1: 'foo',
-					Property2: [ { target: '00000000-0000-0000-0000-000000000001' } ],
+					Property2: { target: '00000000-0000-0000-0000-000000000001' },
 					Property3: [ { target: '00000000-0000-0000-0000-000000000002' }, { target: '00000000-0000-0000-0000-000000000003' } ],
 					Property4: 'bar'
 				}
