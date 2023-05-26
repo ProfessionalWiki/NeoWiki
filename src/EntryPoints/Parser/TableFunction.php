654ef32a38c48f50637f3df8cb3f0b3a4a9fd281
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-05-27 00:59:24 +0200

    Child subject table prototype
    For https://github.com/ProfessionalWiki/NeoWiki/issues/327

---
+++ src/EntryPoints/Parser/TableFunction.php
@@ -0,0 +1,32 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\EntryPoints\Parser;
+
+use Html;
+use Parser;
+
+class TableFunction {
+
+	/**
+	 * @return array<mixed, mixed>
+	 */
+	public function handleParserFunctionCall( Parser $parser, string ...$arguments ): array {
+		$parser->getOutput()->addModules( [ 'ext.neowiki.table-editor' ] );
+
+		return [
+			Html::element(
+				'div',
+				[
+					'class' => 'nwSubjectTableLoader',
+					'data-schema-id' => $arguments[0],
+					'data-page-id' => \Title::newFromDBkey( $parser->getPage()?->getDBkey() )->getId()
+				]
+			),
+			'noparse' => true,
+			'isHTML' => true,
+		];
+	}
+
+}
