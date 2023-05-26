55862749211ce29a6bd36d874c2cd34c2fb69574
Morne Alberts <morne@malberts.net>
2023-05-26 11:51:06 +0200

    Replace test Data with DemoData

--- Neo/tests/Data/TestData.php
+++ Neo/tests/Data/TestData.php
@@ -7,7 +7,7 @@ namespace ProfessionalWiki\NeoWiki\Tests\Data;
 class TestData {
 
 	public static function getFileContents( string $fileName ): string {
-		return file_get_contents( __DIR__ . '/' . $fileName );
+		return file_get_contents( __DIR__ . '/../../DemoData/' . $fileName );
 	}
 
 }
