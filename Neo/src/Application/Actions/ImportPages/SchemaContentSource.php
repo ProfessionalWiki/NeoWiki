9b2e3e52fb07369f421e4c379e3501e1633ac500
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-06-16 16:19:32 +0200

    Extract SchemaContentSource

---
+++ Neo/src/Application/Actions/ImportPages/SchemaContentSource.php
@@ -0,0 +1,44 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\Application\Actions\ImportPages;
+
+use DirectoryIterator;
+use FileFetcher\FileFetcher;
+use ProfessionalWiki\NeoWiki\EntryPoints\Content\SchemaContent;
+
+class SchemaContentSource {
+
+	public function __construct(
+		private readonly string $directoryPath,
+		private readonly FileFetcher $fileFetcher,
+	) {
+	}
+
+	/**
+	 * @return array<string, SchemaContent>
+	 */
+	public function getSchemas(): array {
+		$schemaContents = [];
+
+		$directoryIterator = new DirectoryIterator( $this->directoryPath );
+
+		foreach ( $directoryIterator as $fileInfo ) {
+			if ( !$fileInfo->isFile() ) {
+				continue;
+			}
+
+			$schemaName = $fileInfo->getBasename( '.json' );
+			$schemaContent = $this->getFileContent( $fileInfo->getRealPath() );
+			$schemaContents[$schemaName] = new SchemaContent( $schemaContent );
+		}
+
+		return $schemaContents;
+	}
+
+	private function getFileContent( string $fileName ): string {
+		return $this->fileFetcher->fetchFile( $fileName );
+	}
+
+}
