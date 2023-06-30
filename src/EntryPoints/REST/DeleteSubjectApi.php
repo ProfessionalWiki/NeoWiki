9978876d225c9a23d644e33be360a6996bbf52f7
Tommy <6966950+Tommyshub@users.noreply.github.com>
2023-06-30 17:45:42 +0200

    Fix bug in DeleteSubjectApi (#539)

--- src/EntryPoints/REST/DeleteSubjectApi.php
+++ src/EntryPoints/REST/DeleteSubjectApi.php
@@ -6,29 +6,23 @@ namespace ProfessionalWiki\NeoWiki\EntryPoints\REST;
 
 use MediaWiki\Rest\Response;
 use MediaWiki\Rest\SimpleHandler;
-use ProfessionalWiki\NeoWiki\Application\Actions\DeleteSubject\DeleteSubjectAction;
 use ProfessionalWiki\NeoWiki\Domain\Subject\SubjectId;
+use ProfessionalWiki\NeoWiki\NeoWikiExtension;
 use Wikimedia\ParamValidator\ParamValidator;
 
 class DeleteSubjectApi extends SimpleHandler {
-
-	public function __construct(
-		private DeleteSubjectAction $deleteSubjectAction
-	) {
-	}
-
 	public function run( string $subjectId ): Response {
 		try {
-			$this->deleteSubjectAction->deleteSubject(
+			NeoWikiExtension::getInstance()->newDeleteSubjectAction( $this->getAuthority() )->deleteSubject(
 				new SubjectId( $subjectId )
-			);	
+			);
 		} catch ( \RuntimeException $e ) {
 			return $this->getResponseFactory()->createJson( [
 				'status' => 'error',
 				'message' => $e->getMessage(),
 			] );
 		}
-		
+
 		return new Response();
 	}
 
