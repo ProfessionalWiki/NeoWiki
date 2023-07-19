a064b9a1f67379c784d6d9bca01acb4246cee716
Morne Alberts <morne@malberts.net>
2023-07-20 01:15:43 +0200

    Fix form submission Value handling (#584)
    * Fix form submission Value handling
    
    * Add tests
    
    * Replace ValueMap with StatementList

--- Neo/resources/table-editor/src/editor/domain/__tests__/Value.spec.ts
+++ Neo/resources/table-editor/src/editor/domain/__tests__/Value.spec.ts
@@ -6,8 +6,10 @@ import {
 	newStringValue,
 	Relation,
 	RelationValue,
+	type Value,
+	valueToJson,
 	ValueType
-} from '../Value';
+} from '@/editor/domain/Value';
 
 describe( 'jsonToValue', () => {
 
@@ -96,6 +98,76 @@ describe( 'jsonToValue', () => {
 		const json: string[] = [];
 
 		expect( () => jsonToValue( json, undefined ) ).toThrow();
+	} );
+
+} );
+
+describe( 'valueToJson', () => {
+
+	it( 'converts a StringValue with one string into an array of strings', () => {
+		const value = newStringValue( 'test' );
+
+		const json = valueToJson( value );
+
+		expect( json ).toEqual( [ 'test' ] );
+	} );
+
+	it( 'converts StringValue with multiple strings into an array of strings', () => {
+		const value = newStringValue( 'test1', 'test2' );
+
+		const json = valueToJson( value );
+
+		expect( json ).toEqual( [ 'test1', 'test2' ] );
+	} );
+
+	it( 'converts a NumberValue into a single number', () => {
+		const value = newNumberValue( 123 );
+
+		const json = valueToJson( value );
+
+		expect( json ).toEqual( 123 );
+	} );
+
+	it( 'converts a BooleanValue into a boolean', () => {
+		const value = newBooleanValue( true );
+
+		const json = valueToJson( value );
+
+		expect( json ).toEqual( true );
+	} );
+
+	it( 'converts a RelationValue with a single relation into an array of objects', () => {
+		const value = new RelationValue( [
+			new Relation( 'testId', 'testTarget' )
+		] );
+
+		const json = valueToJson( value );
+
+		expect( json ).toEqual( [
+			{ id: 'testId', target: 'testTarget' }
+		] );
+	} );
+
+	it( 'converts a RelationValue with multiple relations into an array of objects', () => {
+		const value = new RelationValue( [
+			new Relation( 'testId1', 'testTarget1' ),
+			new Relation( 'testId2', 'testTarget2' )
+		] );
+
+		const json = valueToJson( value );
+
+		expect( json ).toEqual( [
+			{ id: 'testId1', target: 'testTarget1' },
+			{ id: 'testId2', target: 'testTarget2' }
+		] );
+	} );
+
+	it( 'throws an error when value is of unexpected type', () => {
+		const value = {
+			type: 'test' as ValueType
+		} as unknown as Value;
+
+		expect( () => valueToJson( value ) ).toThrow( 'Unsupported value type: test' );
 	} );
 
 } );
