fe51cb3ee9007bc185ff09ce0648578b3f23b24c
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-07-26 18:40:56 +0200

    Add some page meta data to Neo4j
    * creation time
    * last editor
    * categories (WIP)
    
    For https://github.com/ProfessionalWiki/NeoWiki/issues/10
    
    Manually tested. Not adding automated tests for now for priotization
    reasons, so this adds some prudent technical debt.

--- Neo/src/Domain/Page/PageProperties.php
+++ Neo/src/Domain/Page/PageProperties.php
@@ -6,8 +6,17 @@ namespace ProfessionalWiki\NeoWiki\Domain\Page;
 
 class PageProperties {
 
+	/**
+	 * @param string $creationTime In the standard MediaWiki format, ie 20230726163439
+	 * @param string $modificationTime In the standard MediaWiki format, ie 20230726163439
+	 * @param string[] $categories
+	 */
 	public function __construct(
-		public readonly string $title
+		public readonly string $title,
+		public readonly string $creationTime,
+		public readonly string $modificationTime,
+		public array $categories,
+		public readonly string $lastEditor
 	) {
 	}
 
