2acd7dddaac15685492ea7655d2b2bf3aba246b6
Tommy <6966950+Tommyshub@users.noreply.github.com>
2023-08-04 22:57:01 +0200

    Allow decimal values for number and currency (#603)

--- Neo/resources/table-editor/src/editor/domain/valueFormats/Currency.ts
+++ Neo/resources/table-editor/src/editor/domain/valueFormats/Currency.ts
@@ -2,7 +2,7 @@ import type { PropertyDefinition } from '@/editor/domain/PropertyDefinition';
 import { newNumberValue, type NumberValue, ValueType } from '@/editor/domain/Value';
 import { CurrencyInputWidgetFactory } from '@/editor/presentation/Widgets/CurrencyWidgetFactory';
 import type { CurrencyInputWidget } from '@/editor/presentation/Widgets/CurrencyWidgetFactory';
-import { BaseValueFormat, ValidationResult } from '@/editor/domain/ValueFormat';
+import { BaseValueFormat, ValidationResult, type ValidationError } from '@/editor/domain/ValueFormat';
 import type { FieldData } from '@/editor/presentation/SchemaForm';
 
 export interface CurrencyProperty extends PropertyDefinition {
@@ -19,8 +19,33 @@ export class CurrencyFormat extends BaseValueFormat<CurrencyProperty, NumberValu
 	public static readonly valueType = ValueType.Number;
 	public static readonly formatName = 'currency';
 
+	private getBoundsMessage( value: number, minimum: number | undefined, maximum: number | undefined ): string | null {
+		if ( minimum !== undefined && maximum !== undefined ) {
+			if ( value < minimum || value > maximum ) {
+				return `Value should be within the allowed range (${minimum} - ${maximum}).`;
+			}
+		}
+
+		if ( minimum !== undefined && value < minimum ) {
+			return `Value should be greater than or equal to ${minimum}.`;
+		}
+
+		if ( maximum !== undefined && value > maximum ) {
+			return `Value should be less than or equal to ${maximum}.`;
+		}
+
+		return null;
+	}
+
 	public validate( value: NumberValue, property: CurrencyProperty ): ValidationResult {
-		return new ValidationResult( [] ); // TODO
+		const errors: ValidationError[] = [];
+		const boundsMessage = this.getBoundsMessage( value.number, property.minimum, property.maximum );
+
+		if ( boundsMessage ) {
+			errors.push( { message: boundsMessage } );
+		}
+
+		return new ValidationResult( errors );
 	}
 
 	public createPropertyDefinitionFromJson( base: PropertyDefinition, json: any ): CurrencyProperty {
@@ -37,7 +62,6 @@ export class CurrencyFormat extends BaseValueFormat<CurrencyProperty, NumberValu
 		return CurrencyInputWidgetFactory.create( {
 			value: value === undefined ? '' : value.number.toString(),
 			currency: property.currencyCode,
-			precision: property.precision,
 			min: property.minimum,
 			max: property.maximum,
 			required: property.required
@@ -45,10 +69,12 @@ export class CurrencyFormat extends BaseValueFormat<CurrencyProperty, NumberValu
 	}
 
 	public async getFieldData( field: CurrencyInputWidget ): Promise<FieldData> {
+		const isValid = ( field.$input[ 0 ] as HTMLInputElement ).checkValidity();
+
 		return {
 			value: newNumberValue( Number( field.getValue() ) ),
-			valid: true,
-			errorMessage: undefined
+			valid: isValid,
+			errorMessage: isValid ? undefined : ( field.$input[ 0 ] as HTMLInputElement ).validationMessage
 		};
 	}
 }
