ad783ba8a6d942df384d2cb602284898bbe5cd6f
Andrei Petushok <apetushok@gmail.com>
2023-08-17 17:31:59 +0100

    Add neowiki SearchEngine method

--- Neo/src/Application/Queries/GetSchemaNamesQuery.php
+++ Neo/src/Application/Queries/GetSchemaNamesQuery.php
@@ -3,18 +3,36 @@
 namespace ProfessionalWiki\NeoWiki\Application\Queries;
 
 use ProfessionalWiki\NeoWiki\Persistence\MediaWiki\DatabaseSchemaNameLookup\DatabaseSchemaNameLookup;
+use SearchEngine;
+use Title;
 
 class GetSchemaNamesQuery {
+	private const LIMIT = 10;
 
 	public function __construct(
-		private readonly DatabaseSchemaNameLookup $schemaNameLookup
+		private readonly DatabaseSchemaNameLookup $schemaNameLookup,
+		private readonly SearchEngine $searchEngine
 	) {
 	}
 
 	public function execute( string $search ): array {
-		$schemaNames = $this->schemaNameLookup->getFirstTenSchemaNamesMatching( $search );
-		return array_map( function ( array $schemaName ): string {
-			return (string)$schemaName[ 'page_title' ];
+		$schemaNames = $this->getSchemasName( $search );
+
+		return array_map( function ( Title $title ): string {
+			return $title->getText();
 		}, $schemaNames );
+	}
+
+	private function getSchemasName( string $search ): array {
+		if ( $search ) {
+			$this->searchEngine->setNamespaces( [ (int)NS_NEOWIKI_SCHEMA ] );
+			$this->searchEngine->setLimitOffset( $this::LIMIT );
+
+			return $this->searchEngine->extractTitles(
+				$this->searchEngine->completionSearch( $search )
+			);
+		}
+
+		return $this->schemaNameLookup->getFirstTenSchemaNames();
 	}
 }
