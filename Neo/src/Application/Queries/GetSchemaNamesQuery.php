8a2123ef98510d1672fc652a62dc293c81bda092
Andrei Petushok <apetushok@gmail.com>
2023-08-18 10:04:24 +0100

    Add review fixing

--- Neo/src/Application/Queries/GetSchemaNamesQuery.php
+++ Neo/src/Application/Queries/GetSchemaNamesQuery.php
@@ -2,37 +2,20 @@
 
 namespace ProfessionalWiki\NeoWiki\Application\Queries;
 
-use ProfessionalWiki\NeoWiki\Persistence\MediaWiki\DatabaseSchemaNameLookup\DatabaseSchemaNameLookup;
-use SearchEngine;
+use ProfessionalWiki\NeoWiki\Persistence\MediaWiki\DatabaseSchemaNameLookup\SchemaNameLookup;
 use Title;
 
 class GetSchemaNamesQuery {
-	private const LIMIT = 10;
-
 	public function __construct(
-		private readonly DatabaseSchemaNameLookup $schemaNameLookup,
-		private readonly SearchEngine $searchEngine
+		private readonly SchemaNameLookup $schemaNameLookup
 	) {
 	}
 
 	public function execute( string $search ): array {
-		$schemaNames = $this->getSchemasName( $search );
+		$schemaNames = $this->schemaNameLookup->getSchemasNameMatching( $search );
 
 		return array_map( function ( Title $title ): string {
 			return $title->getText();
 		}, $schemaNames );
-	}
-
-	private function getSchemasName( string $search ): array {
-		if ( $search ) {
-			$this->searchEngine->setNamespaces( [ (int)NS_NEOWIKI_SCHEMA ] );
-			$this->searchEngine->setLimitOffset( $this::LIMIT );
-
-			return $this->searchEngine->extractTitles(
-				$this->searchEngine->completionSearch( $search )
-			);
-		}
-
-		return $this->schemaNameLookup->getFirstTenSchemaNames();
 	}
 }
