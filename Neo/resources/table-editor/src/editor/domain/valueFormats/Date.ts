0cd6b4d157b1f0bc94961f5e0c877621e6104831
apetushok <apetushok@gmail.com>
2023-08-23 21:59:51 +0100

    Add date widget position fix (#660)
    * Add date widget position fix
    
    * Add removing interface
    
    * Add factories for date widgets

--- Neo/resources/table-editor/src/editor/domain/valueFormats/Date.ts
+++ Neo/resources/table-editor/src/editor/domain/valueFormats/Date.ts
@@ -2,6 +2,7 @@ import type { PropertyDefinition } from '@/editor/domain/PropertyDefinition';
 import { newStringValue, type StringValue, ValueType } from '@/editor/domain/Value';
 import { BaseValueFormat, ValidationResult } from '@/editor/domain/ValueFormat';
 import type { FieldData } from '@/editor/presentation/SchemaForm';
+import type { DateWidgetFactory } from '@/editor/presentation/Widgets/DateWidgets/DateWidgetFactory';
 
 export interface DateProperty extends PropertyDefinition {
 }
@@ -10,6 +11,10 @@ export class DateFormat extends BaseValueFormat<DateProperty, StringValue, OO.ui
 
 	public static readonly valueType = ValueType.String;
 	public static readonly formatName = 'date';
+
+	public constructor( private readonly dateWidgetFactory: DateWidgetFactory ) {
+		super();
+	}
 
 	public validate( value: StringValue, property: DateProperty ): ValidationResult {
 		return new ValidationResult( [] ); // TODO
@@ -22,21 +27,11 @@ export class DateFormat extends BaseValueFormat<DateProperty, StringValue, OO.ui
 	}
 
 	public createFormField( value: StringValue | undefined, property: DateProperty ): OO.ui.Widget {
-		const widget = new mw.widgets.DateInputWidget( { // TODO: handle multiple values?
+		return this.dateWidgetFactory.create( { // TODO: handle multiple values?
 			displayFormat: 'Do [of] MMMM, YYYY',
 			value: value?.strings[ 0 ] ?? '',
 			required: property.required
 		} );
-
-		widget.$element.find( '.mw-widget-dateInputWidget-calendar' ).css( {
-			position: 'relative'
-		} );
-
-		widget.$element.find( '.mw-widget-dateInputWidget-calendar .mw-widget-calendarWidget-body' ).css( {
-			position: 'absolute'
-		} );
-
-		return widget;
 	}
 
 	public async getFieldData( field: OO.ui.InputWidget ): Promise<FieldData> {
