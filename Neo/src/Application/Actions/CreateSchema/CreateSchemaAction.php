e5ebd8db10d8370fc86dc6a637e765c35d6e215b
apetushok <apetushok@gmail.com>
2023-09-22 16:04:34 +0100

    Add REST endpoint to create schemas (#752)

---
+++ Neo/src/Application/Actions/CreateSchema/CreateSchemaAction.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace ProfessionalWiki\NeoWiki\Application\Actions\CreateSchema;
+
+use MediaWiki\Revision\SlotRecord;
+use ProfessionalWiki\NeoWiki\Domain\Schema\SchemaLookup;
+use ProfessionalWiki\NeoWiki\Domain\Schema\SchemaName;
+use ProfessionalWiki\NeoWiki\EntryPoints\Content\SchemaContent;
+use ProfessionalWiki\NeoWiki\Infrastructure\SchemaAuthorizer;
+use ProfessionalWiki\NeoWiki\Persistence\MediaWiki\PageContentSaver;
+use ProfessionalWiki\NeoWiki\Persistence\MediaWiki\PageContentSavingStatus;
+use ProfessionalWiki\NeoWiki\Persistence\MediaWiki\SchemaPersistenceDeserializer;
+
+class CreateSchemaAction {
+	public function __construct(
+		private readonly CreateSchemaPresenter $presenter,
+		private readonly PageContentSaver $pageContentSaver,
+		private readonly SchemaLookup $schemaLookup,
+		private readonly SchemaPersistenceDeserializer $deserializer,
+		private readonly SchemaAuthorizer $authorizer
+	) {
+	}
+
+	public function execute( string $name, string $schemaJson ): void {
+		if ( !$this->authorizer->canCreateSchema() ) {
+			$this->presenter->presentPermissionsDenied();
+			return;
+		}
+
+		$schemaName = new SchemaName( $name );
+
+		$existingSchema = $this->schemaLookup->getSchema( $schemaName );
+		if ( $existingSchema !== null ) {
+			$this->presenter->presentSchemaAlreadyExists();
+			return;
+		}
+
+		try {
+			$this->deserializer->deserialize( $schemaName,  $schemaJson );
+		} catch ( \Exception $e ) {
+			$this->presenter->presentInvalidArguments();
+			return;
+		}
+
+		$title = \Title::newFromText( 'Schema:' . $name );
+		if ( $title === null ) {
+			$this->presenter->presentInvalidTitle();
+			return;
+		}
+
+		$savingResult = $this->pageContentSaver->saveContent(
+			page: $title,
+			contentBySlot: [ SlotRecord::MAIN => new SchemaContent( $schemaJson ) ],
+			comment: \CommentStoreComment::newUnsavedComment( 'The schema is created' )
+		);
+
+		if ( $savingResult->status === PageContentSavingStatus::ERROR ) {
+			$this->presenter->presentSchemaCreationError( $savingResult->errorMessage );
+			return;
+		}
+
+		if ( $savingResult->status === PageContentSavingStatus::NO_CHANGES ) {
+			$this->presenter->presentNoChanges();
+			return;
+		}
+
+		if ( $savingResult->status === PageContentSavingStatus::REVISION_CREATED ) {
+			$this->presenter->presentSchema( $schemaJson );
+		}
+	}
+}
