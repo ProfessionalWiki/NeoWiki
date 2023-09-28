d87070ddca2efc4a8d1cbf4cd770b26df874cbd3
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-09-28 14:58:16 +0200

    Use format registry in statementsToNodeProperties
    For https://github.com/ProfessionalWiki/NeoWiki/issues/702

--- src/Persistence/Neo4j/SubjectUpdaterFactory.php
+++ src/Persistence/Neo4j/SubjectUpdaterFactory.php
@@ -7,21 +7,24 @@ namespace ProfessionalWiki\NeoWiki\Persistence\Neo4j;
 use Laudis\Neo4j\Contracts\TransactionInterface;
 use ProfessionalWiki\NeoWiki\Domain\Page\PageId;
 use ProfessionalWiki\NeoWiki\Domain\Schema\SchemaLookup;
+use ProfessionalWiki\NeoWiki\Domain\ValueFormat\ValueFormatLookup;
 use Psr\Log\LoggerInterface;
 
 class SubjectUpdaterFactory {
 
 	public function __construct(
 		private readonly SchemaLookup $schemaLookup,
+		private readonly ValueFormatLookup $valueFormatLookup,
 		private readonly LoggerInterface $logger
 	) {
 	}
 
 	public function newSubjectUpdater( TransactionInterface $transaction, PageId $pageId ): SubjectUpdater {
 		return new SubjectUpdater(
-			$this->schemaLookup,
 			$transaction,
 			$pageId,
+			$this->schemaLookup,
+			$this->valueFormatLookup,
 			$this->logger
 		);
 	}
