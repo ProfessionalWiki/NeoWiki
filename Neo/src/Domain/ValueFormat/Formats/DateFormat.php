65eda20181a55d837544189d984c48ce9c1324eb
Jeroen De Dauw <jeroendedauw@gmail.com>
2023-09-29 00:22:26 +0200

    Store dates as Date in Neo4j
    Part of https://github.com/ProfessionalWiki/NeoWiki/issues/702

--- Neo/src/Domain/ValueFormat/Formats/DateFormat.php
+++ Neo/src/Domain/ValueFormat/Formats/DateFormat.php
@@ -4,15 +4,20 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\Domain\ValueFormat\Formats;
 
+use DateTimeImmutable;
+use Laudis\Neo4j\Types\CypherList;
+use Laudis\Neo4j\Types\Date;
 use ProfessionalWiki\NeoWiki\Domain\Schema\Property\DateProperty;
 use ProfessionalWiki\NeoWiki\Domain\Schema\PropertyCore;
 use ProfessionalWiki\NeoWiki\Domain\Value\NeoValue;
+use ProfessionalWiki\NeoWiki\Domain\Value\StringValue;
 use ProfessionalWiki\NeoWiki\Domain\Value\ValueType;
 use ProfessionalWiki\NeoWiki\Domain\ValueFormat\ValueFormat;
 
 class DateFormat implements ValueFormat {
 
 	public const NAME = 'date';
+	private const SECONDS_PER_DAY = 86400;
 
 	public function getFormatName(): string {
 		return self::NAME;
@@ -27,7 +32,24 @@ class DateFormat implements ValueFormat {
 	}
 
 	public function buildNeo4jValue( NeoValue $value ): mixed {
-		return $value->toScalars(); // TODO
+		/**
+		 * @var StringValue $value
+		 */
+
+		$dates = [];
+
+		foreach ( $value->strings as $string ) {
+			try {
+				$dates[] = new Date(
+					days: intdiv( ( new DateTimeImmutable( $string ) )->getTimestamp(), self::SECONDS_PER_DAY )
+				);
+			}
+			catch ( \Exception ) {
+				// Ignore invalid dates
+			}
+		}
+
+		return new CypherList( $dates );
 	}
 
 }
