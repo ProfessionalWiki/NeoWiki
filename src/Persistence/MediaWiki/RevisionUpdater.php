57a72a5c9cd9c025a42199cc05dd5c04d54c859f
apetushok <apetushok@gmail.com>
2023-10-21 14:45:46 +0100

    Save subjects upon Visual Editor save (#852)
    * Store small refactoring
    
    * Prepare neoWikiStore and use it in InfoboxManagerStore
    
    * Front end bulk subjects creating (not fully completed)
    
    * Finalizing the front end of implementation
    
    * Small refactoring after review
    
    * Small fixes after review
    
    * Test for LoadingIndicator
    
    * Style fixes
    
    * Test fix
    
    * Add VE save request parameter changes
    
    * Partial implementation of the subjects saving of server part
    
    * Saving of subjects in the current revision.
    
    Style fixes
    
    * Cache clearing after page editing
    
    * Add tests
    
    ---------
    
    Co-authored-by: Jeroen De Dauw <jeroendedauw@gmail.com>

---
+++ src/Persistence/MediaWiki/RevisionUpdater.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ProfessionalWiki\NeoWiki\Persistence\MediaWiki;
+
+use MediaWiki\Revision\MutableRevisionRecord;
+use MediaWiki\Revision\RenderedRevision;
+use User;
+use Content;
+use CommentStoreComment;
+use MWTimestamp;
+
+class RevisionUpdater {
+
+	public function __construct(
+		private readonly User $performer,
+		private readonly RenderedRevision $renderedRevision
+	) {
+	}
+
+	/**
+	 * @param array<string, Content> $contentBySlot Keys are slot names, values are Content objects
+	 */
+	public function addSubjectsToRevision( array $contentBySlot ): void {
+		/** @var MutableRevisionRecord $revRecord */
+		$revRecord = $this->renderedRevision->getRevision();
+
+		foreach ( $contentBySlot as $slotName => $content ) {
+			$revRecord->setContent( $slotName, $content );
+		}
+
+		$revRecord->setUser( $this->performer )
+			->setComment( CommentStoreComment::newUnsavedComment( 'New subjects added' ) )
+			->setTimestamp( MWTimestamp::now( (int)TS_MW ) );
+
+		$this->renderedRevision->updateRevision( $revRecord );
+	}
+}
