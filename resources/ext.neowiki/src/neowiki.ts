61be45fd135a09c4873886d5e392c46ca64a8df9
Morne Alberts <morne@malberts.net>
2024-09-17 18:48:56 +0200

    Use Vue Teleport

--- resources/ext.neowiki/src/neowiki.ts
+++ resources/ext.neowiki/src/neowiki.ts
@@ -1,48 +1,7 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-import { createMwApp, h } from 'vue';
+import { createMwApp } from 'vue';
 import { createPinia } from 'pinia';
-import { useNeoWikiStore } from '@/stores/Store';
-import AutomaticInfobox from '@/components/AutomaticInfobox.vue';
-import CreateSubjectButton from '@/components/CreateSubject/CreateSubjectButton.vue';
+import NeoWikiApp from '@/components/NeoWikiApp.vue';
 
-const app = createMwApp( {
-	setup() {
-		const store = useNeoWikiStore();
-		return { store };
-	},
-	render() {
-		const infoboxes = document.querySelectorAll( '.neowiki-infobox' );
-		const components = Array.from( infoboxes ).map( ( el, index ) => h( AutomaticInfobox, {
-			key: index,
-			ref: ( instance ) => {
-				if ( instance !== null ) {
-					el.appendChild( ( instance as any ).$el );
-				}
-			},
-			// TODO: Remove
-			title: 'Foo',
-			statements: [
-				{ property: 'Foo', value: 'Bar' },
-				{ property: 'Lorem', value: 'Ipsum' }
-			]
-		} ) );
-
-		const createButton = document.getElementById( 'mw-indicator-neowiki-create-button' );
-		if ( createButton ) {
-			components.push( h( CreateSubjectButton, {
-				key: 'create-button',
-				ref: ( instance ) => {
-					if ( instance !== null ) {
-						createButton.appendChild( ( instance as any ).$el );
-					}
-				}
-			} ) );
-		}
-
-		return components;
-	}
-} );
-
+const app = createMwApp( NeoWikiApp );
 app.use( createPinia() );
 app.mount( '#neowiki' );
