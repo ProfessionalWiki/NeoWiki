d773fce98f587c1b15ca41ca4927180198f6271b
Morne Alberts <morne@malberts.net>
2024-09-19 12:52:26 +0200

    Remove neo package and use path instead

--- resources/ext.neowiki/src/neowiki.ts
+++ resources/ext.neowiki/src/neowiki.ts
@@ -1,7 +1,11 @@
 import { createMwApp } from 'vue';
 import { createPinia } from 'pinia';
 import NeoWikiApp from '@/components/NeoWikiApp.vue';
+import { Neo } from '@neo/Neo.ts';
 
 const app = createMwApp( NeoWikiApp );
 app.use( createPinia() );
 app.mount( '#neowiki' );
+
+// TODO: this is just to include Neo code in the build. Remove when actually using it.
+Neo.getInstance();
