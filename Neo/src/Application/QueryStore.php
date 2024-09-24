752a5afa7a3c646ec87c570caa5af5f841c598d5
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-09-25 01:55:38 +0200

    Add QueryStore and WriteQueryEngine interfaces

---
+++ Neo/src/Application/QueryStore.php
@@ -0,0 +1,19 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace ProfessionalWiki\NeoWiki\Application;
+
+use ProfessionalWiki\NeoWiki\Domain\Page\Page;
+use ProfessionalWiki\NeoWiki\Domain\Page\PageId;
+
+/**
+ * "Secondary" or "read/query" persistence
+ */
+interface QueryStore {
+
+	public function savePage( Page $page ): void;
+
+	public function deletePage( PageId $pageId ): void;
+
+}
