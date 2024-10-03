fde5fbfae7beed41bdb0a54afbb15a4808c27805
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-10-03 21:10:44 +0200

    Add saveSchema method
    And a SchemaSerializer used by said saveSchema method
    
    For https://github.com/ProfessionalWiki/NeoExtension/issues/99

---
+++ resources/ext.neowiki/src/persistence/SchemaSerializer.ts
@@ -0,0 +1,30 @@
+import { Schema } from '@neo/domain/Schema';
+import { PropertyDefinitionList } from '@neo/domain/PropertyDefinitionList';
+import { PropertyDefinition } from '@neo/domain/PropertyDefinition';
+
+export class SchemaSerializer {
+
+	public serializeSchema( schema: Schema ): string {
+		return JSON.stringify(
+			{
+				description: schema.getDescription(),
+				propertyDefinitions: this.serializePropertyDefinitions( schema.getPropertyDefinitions() )
+			},
+			null,
+			4
+		);
+	}
+
+	private serializePropertyDefinitions( propertyDefinitions: PropertyDefinitionList ): Record<string, any> {
+		const serializedDefinitions: Record<string, any> = {};
+		for ( const property of propertyDefinitions ) {
+			serializedDefinitions[ property.name.toString() ] = this.serializePropertyDefinition( property );
+		}
+		return serializedDefinitions;
+	}
+
+	private serializePropertyDefinition( property: PropertyDefinition ): any {
+		const { name, ...propertyWithoutName } = property;
+		return propertyWithoutName;
+	}
+}
