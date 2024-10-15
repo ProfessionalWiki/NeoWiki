2e1978060e6c6daa8235544240778fbfd07206ee
Jeroen De Dauw <jeroendedauw@gmail.com>
2024-10-15 18:39:45 +0200

    Improve/add factory functions for PropertyDefinition

--- Neo/neojs/src/domain/valueFormats/Number.ts
+++ Neo/neojs/src/domain/valueFormats/Number.ts
@@ -2,6 +2,7 @@ import type { PropertyDefinition } from '@neo/domain/PropertyDefinition';
 import { PropertyName } from '@neo/domain/PropertyDefinition';
 import { newNumberValue, type NumberValue, ValueType } from '@neo/domain/Value';
 import { BaseValueFormat } from '@neo/domain/ValueFormat';
+import { UrlFormat } from '@neo/domain/valueFormats/Url';
 
 export interface NumberProperty extends PropertyDefinition {
 
@@ -32,11 +33,24 @@ export class NumberFormat extends BaseValueFormat<NumberProperty, NumberValue> {
 
 }
 
-export function newNumberProperty( name = 'MyNumberProperty' ): NumberProperty {
+export function newNumberProperty( options: string|Partial<NumberProperty> = {} ): NumberProperty {
+	if ( typeof options === 'string' ) { // TODO: remove deprecated form
+		return {
+			name: new PropertyName( options ),
+			format: NumberFormat.formatName,
+			description: '',
+			required: false
+		};
+	}
+
 	return {
-		name: new PropertyName( name ),
-		format: NumberFormat.formatName,
-		description: '',
-		required: false
+		name: options.name instanceof PropertyName ? options.name : new PropertyName( options.name || 'number' ),
+		format: UrlFormat.formatName,
+		description: options.description || '',
+		required: options.required || false,
+		default: options.default || undefined,
+		precision: options.precision || undefined,
+		minimum: options.minimum || undefined,
+		maximum: options.maximum || undefined
 	};
 }
--- Neo/neojs/src/domain/valueFormats/Text.ts
+++ Neo/neojs/src/domain/valueFormats/Text.ts
@@ -2,8 +2,13 @@ import type { MultiStringProperty, PropertyDefinition } from '@neo/domain/Proper
 import { PropertyName } from '@neo/domain/PropertyDefinition';
 import { newStringValue, type StringValue, ValueType } from '@neo/domain/Value';
 import { BaseValueFormat } from '@neo/domain/ValueFormat';
+import { UrlFormat } from '@neo/domain/valueFormats/Url';
 
 export interface TextProperty extends MultiStringProperty {
+
+	readonly maxLength?: number;
+	readonly minLength?: number;
+
 }
 
 export class TextFormat extends BaseValueFormat<TextProperty, StringValue> {
@@ -26,14 +31,33 @@ export class TextFormat extends BaseValueFormat<TextProperty, StringValue> {
 
 }
 
-export function newTextProperty( name = 'MyTextProperty', multiple = false, format = TextFormat.formatName ): TextProperty {
+export function newTextProperty(
+	options: string|Partial<TextProperty> = {},
+	name = 'MyTextProperty',
+	multiple = false,
+	format = TextFormat.formatName
+): TextProperty {
+	if ( typeof options === 'string' ) { // TODO: remove deprecated form
+		return {
+			name: new PropertyName( name ),
+			format: format,
+			description: '',
+			required: false,
+			default: newStringValue( '' ),
+			multiple: multiple,
+			uniqueItems: true
+		};
+	}
+
 	return {
-		name: new PropertyName( name ),
-		format: format,
-		description: '',
-		required: false,
-		default: newStringValue( '' ),
-		multiple: multiple,
-		uniqueItems: true
+		name: options.name instanceof PropertyName ? options.name : new PropertyName( options.name || 'text' ),
+		format: UrlFormat.formatName,
+		description: options.description || '',
+		required: options.required || false,
+		default: options.default || undefined,
+		multiple: options.multiple || false,
+		uniqueItems: options.uniqueItems ?? true,
+		maxLength: options.maxLength || undefined,
+		minLength: options.minLength || undefined
 	};
 }
