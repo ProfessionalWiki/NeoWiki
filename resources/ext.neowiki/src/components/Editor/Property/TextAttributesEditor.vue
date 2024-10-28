7ba2b6a19a57f2be2b117ea02e453acdd2c6a100
Morne Alberts <morne@malberts.net>
2024-10-28 19:24:07 +0200

    Add Text attribute fields

--- resources/ext.neowiki/src/components/Editor/Property/TextAttributesEditor.vue
+++ resources/ext.neowiki/src/components/Editor/Property/TextAttributesEditor.vue
@@ -1,14 +1,37 @@
 <template>
-	<div />
-	<!-- TODO -->
+	<div class="text-attributes">
+		<CdxCheckbox
+			:model-value="property.multiple"
+			:label="$i18n( 'neowiki-property-editor-multiple' ).text()"
+			@update:model-value="updateMultiple"
+		>
+			<small>{{ $i18n( 'neowiki-property-editor-multiple' ).text() }}</small>
+		</CdxCheckbox>
+
+		<CdxCheckbox
+			v-if="property.multiple"
+			:model-value="property.uniqueItems"
+			:label="$i18n( 'neowiki-property-editor-unique-items' ).text()"
+			@update:model-value="updateUniqueItems"
+		>
+			<small>{{ $i18n( 'neowiki-property-editor-unique-items' ).text() }}</small>
+		</CdxCheckbox>
+	</div>
 </template>
 
 <script setup lang="ts">
 import { MultiStringProperty } from '@neo/domain/PropertyDefinition.ts';
 import { AttributesEditorEmits, AttributesEditorProps } from '@/components/Editor/Property/AttributesEditorContract.ts';
+import { CdxCheckbox } from '@wikimedia/codex';
 
 defineProps<AttributesEditorProps<MultiStringProperty>>();
-defineEmits<AttributesEditorEmits<MultiStringProperty>>();
+const emit = defineEmits<AttributesEditorEmits<MultiStringProperty>>();
 
-// TODO: emit like in NumberAttributesEditor.vue
+const updateMultiple = ( value: boolean ): void => {
+	emit( 'update:property', { multiple: value } );
+};
+
+const updateUniqueItems = ( value: boolean ): void => {
+	emit( 'update:property', { uniqueItems: value } );
+};
 </script>
