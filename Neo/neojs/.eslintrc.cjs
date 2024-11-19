fb481df7ad3f9737325a3de3651f34b922300df6
Waseem Abbas <waximabbax@gmail.com>
2024-11-20 03:51:22 +0500

    throw eslint error for func-style in neojs

--- Neo/neojs/.eslintrc.cjs
+++ Neo/neojs/.eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
 		sourceType: 'module'
 	},
 	rules: {
-		'func-style': [ 'warn', 'declaration', { allowArrowFunctions: false } ],
+		'func-style': [ 'error', 'declaration', { allowArrowFunctions: false } ],
 		// These @typescript-eslint rules are disabled because they are replaced by the @stylistic rules.
 		'@typescript-eslint/indent': 'off',
 		'@typescript-eslint/member-delimiter-style': 'off',
