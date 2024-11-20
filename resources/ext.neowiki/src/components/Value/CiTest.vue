eab8c2319d96a859e0df3d2bba3b44a23f0eab05
Waseem Abbas <waximabbax@gmail.com>
2024-11-20 05:25:28 +0500

    Ensure code handles new files

---
+++ resources/ext.neowiki/src/components/Value/CiTest.vue
@@ -0,0 +1,8 @@
+<template>
+	<div>TODO: Remove test file after testing</div>
+</template>
+
+<script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const test = (): void => true; // For lint test, TODO: REMOVE
+</script>
