7c29f270a810ae032bbf97539f5bc2b928ac85d7
alistair3149 <alistair31494322@gmail.com>
2025-03-26 12:46:34 -0400

    Fix broken ts-build step

--- resources/ext.neowiki/src/mediawiki-vue.d.ts
+++ resources/ext.neowiki/src/mediawiki-vue.d.ts
@@ -1,17 +1,10 @@
 import { Vue } from 'vue';
 import { Component } from '@vue/runtime-core';
-import { mw } from 'types-mediawiki';
 
 declare module '@vue/runtime-core' {
 	export function createMwApp( rootComponent: Component, rootProps?: rootProps ): Vue.App<Element>;
 
 	interface ComponentCustomProperties {
 		$i18n: ( ...args: Parameters<typeof window.mw.message> ) => mw.Message;
-	}
-}
-
-declare global {
-	interface Window {
-		mw: typeof mw;
 	}
 }
