117d71acfe8c7741f726a7071d56bbc3664e6591
Morne Alberts <morne@malberts.net>
2025-04-18 18:57:30 +0200

    Add Edit Schema Button entrypoint

--- resources/ext.neowiki/src/neowiki.ts
+++ resources/ext.neowiki/src/neowiki.ts
@@ -4,6 +4,7 @@ import '@/assets/scss/global.scss';
 import NeoWikiApp from '@/components/NeoWikiApp.vue';
 import { CdxTooltip } from '@wikimedia/codex';
 import { NeoWikiServices } from '@/NeoWikiServices.ts';
+import EditSchemaButton from '@/components/EditSchemaButton.vue';
 
 const automaticInfobox = document.querySelector( '#neowiki' );
 if ( automaticInfobox !== null ) {
@@ -11,4 +12,11 @@ if ( automaticInfobox !== null ) {
 	app.use( createPinia() );
 	NeoWikiServices.registerServices( app );
 	app.mount( automaticInfobox );
+}
+
+const editSchema = document.querySelector( '#ext-neowiki-edit-schema' );
+if ( editSchema !== null ) {
+	const app = createMwApp( EditSchemaButton );
+	NeoWikiServices.registerServices( app );
+	app.mount( editSchema );
 }
