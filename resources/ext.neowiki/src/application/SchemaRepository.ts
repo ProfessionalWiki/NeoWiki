b410d838a597ce8f77126f2b83e0121ba6112c31
Morne Alberts <morne@malberts.net>
2025-05-14 15:15:36 +0200

    Add InMemorySchemaRepository

--- resources/ext.neowiki/src/application/SchemaRepository.ts
+++ resources/ext.neowiki/src/application/SchemaRepository.ts
@@ -1,8 +1,16 @@
-import type { SchemaLookup } from '@/application/SchemaLookup';
+import { InMemorySchemaLookup, SchemaLookup } from '@/application/SchemaLookup';
 import { Schema } from '@neo/domain/Schema.ts';
 
 export interface SchemaRepository extends SchemaLookup {
 
 	saveSchema( schema: Schema ): Promise<void>;
+
+}
+
+export class InMemorySchemaRepository extends InMemorySchemaLookup implements SchemaRepository {
+
+	public async saveSchema( schema: Schema ): Promise<void> {
+		this.schemas.set( schema.getName(), schema );
+	}
 
 }
