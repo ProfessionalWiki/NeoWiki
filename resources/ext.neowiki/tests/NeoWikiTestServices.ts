76da0e000dca3a75bce138f2b81ad87bb9aedd24
Morne Alberts <morne@malberts.net>
2025-05-14 15:15:49 +0200

    Add test services

---
+++ resources/ext.neowiki/tests/NeoWikiTestServices.ts
@@ -0,0 +1,20 @@
+import { NeoWikiServices, Service } from '@/NeoWikiServices.ts';
+import { NeoWikiExtension } from '@/NeoWikiExtension.ts';
+import { InMemorySchemaRepository } from '@/application/SchemaRepository.ts';
+
+export class NeoWikiTestServices extends NeoWikiServices {
+
+	public static getServices(): Record<string, unknown> {
+		const neoWiki = NeoWikiExtension.getInstance();
+
+		return {
+			[ Service.ComponentRegistry ]: neoWiki.getTypeSpecificComponentRegistry(),
+			[ Service.SchemaAuthorizer ]: neoWiki.newSchemaAuthorizer(),
+			[ Service.SubjectAuthorizer ]: neoWiki.newSubjectAuthorizer(),
+			[ Service.SubjectValidator ]: neoWiki.newSubjectValidator(),
+			[ Service.PropertyTypeRegistry ]: neoWiki.getPropertyTypeRegistry(),
+			[ Service.SchemaRepository ]: new InMemorySchemaRepository( [] )
+		};
+	}
+
+}
