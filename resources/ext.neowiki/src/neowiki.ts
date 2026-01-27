6c94065bf545bce6564d47b568606889fec62606
alistair3149 <alistair31494322@gmail.com>
2026-01-27 13:07:56 -0500

    Remove unused global SCSS styles

--- resources/ext.neowiki/src/neowiki.ts
+++ resources/ext.neowiki/src/neowiki.ts
@@ -1,6 +1,5 @@
 import { createMwApp } from 'vue';
 import { createPinia } from 'pinia';
-import '@/assets/scss/global.scss';
 import NeoWikiApp from '@/components/NeoWikiApp.vue';
 import { CdxTooltip } from '@wikimedia/codex';
 import { NeoWikiServices } from '@/NeoWikiServices.ts';
