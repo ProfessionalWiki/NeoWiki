2ebad297dcc9f4d18fd29d88b9ab5f6c923212f5
alistair3149 <alistair3149@users.noreply.github.com>
2026-01-30 12:21:11 -0500

    Implement subject creation with existing schema (#477)

---
+++ resources/ext.neowiki/src/components/SubjectCreator/SubjectCreator.vue
@@ -0,0 +1,110 @@
+<template>
+	<div class="ext-neowiki-subject-creator">
+		<p>
+			{{ $i18n( 'neowiki-subject-creator-schema-title' ).text() }}
+		</p>
+
+		<CdxToggleButtonGroup
+			v-model="selectedValue"
+			class="ext-neowiki-subject-creator-schema-options"
+			:buttons="buttons"
+		/>
+
+		<div
+			v-if="selectedValue === 'existing'"
+			class="ext-neowiki-subject-creator-existing"
+		>
+			<SchemaLookup
+				ref="schemaLookupRef"
+				@select="onSchemaSelected"
+			/>
+		</div>
+
+		<div v-if="selectedValue === 'new'">
+			TODO: New schema UI
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, watch, nextTick, onMounted } from 'vue';
+import { CdxToggleButtonGroup } from '@wikimedia/codex';
+import { cdxIconSearch, cdxIconAdd } from '@wikimedia/codex-icons';
+import type { ButtonGroupItem } from '@wikimedia/codex';
+import { Subject } from '@/domain/Subject.ts';
+import SchemaLookup from '@/components/SubjectCreator/SchemaLookup.vue';
+
+const emit = defineEmits<{
+	'draft': [ subject: Subject ];
+}>();
+
+const selectedValue = ref( 'existing' );
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const schemaLookupRef = ref<any | null>( null );
+
+const buttons = [
+	{
+		value: 'existing',
+		label: mw.msg( 'neowiki-subject-creator-existing-schema' ),
+		icon: cdxIconSearch
+	},
+	{
+		value: 'new',
+		label: mw.msg( 'neowiki-subject-creator-new-schema' ),
+		icon: cdxIconAdd
+	}
+] as ButtonGroupItem[];
+
+onMounted( () => {
+	focusSchemaLookup( selectedValue.value );
+} );
+
+watch( selectedValue, focusSchemaLookup );
+
+async function focusSchemaLookup( newValue: string ): Promise<void> {
+	await nextTick();
+	if ( newValue === 'existing' && schemaLookupRef.value ) {
+		schemaLookupRef.value.focus();
+	}
+}
+
+async function onSchemaSelected( schemaName: string ): Promise<void> {
+	if ( !schemaName ) {
+		return;
+	}
+
+	const subjectLabel = mw.config.get( 'wgTitle' );
+	if ( typeof subjectLabel !== 'string' ) {
+		mw.notify( 'Error preparing subject: No subject label found', { type: 'error' } );
+		return;
+	}
+
+	try {
+		const subject = Subject.createNew( subjectLabel, schemaName );
+		emit( 'draft', subject );
+	} catch ( error ) {
+		console.error( 'Error preparing subject:', error );
+		mw.notify( 'Error preparing subject: ' + String( error ), { type: 'error' } );
+	}
+}
+</script>
+
+<style lang="less">
+@import ( reference ) '@wikimedia/codex-design-tokens/theme-wikimedia-ui.less';
+
+.ext-neowiki-subject-creator {
+	&-schema-options.cdx-toggle-button-group {
+		width: inherit;
+		display: flex;
+		flex-wrap: wrap;
+
+		.cdx-toggle-button {
+			flex-grow: 1;
+		}
+	}
+
+	&-existing {
+		margin-top: @spacing-100;
+	}
+}
+</style>
