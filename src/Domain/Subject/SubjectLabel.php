d0c4f7bf199a94275573750d63f48c84ab34efbc
alistair3149 <alistair31494322@gmail.com>
2026-05-12 17:33:46 -0400

    Reject empty SubjectLabel

--- src/Domain/Subject/SubjectLabel.php
+++ src/Domain/Subject/SubjectLabel.php
@@ -4,11 +4,16 @@ declare( strict_types = 1 );
 
 namespace ProfessionalWiki\NeoWiki\Domain\Subject;
 
+use InvalidArgumentException;
+
 readonly class SubjectLabel {
 
 	public function __construct(
 		public string $text,
 	) {
+		if ( trim( $text ) === '' ) {
+			throw new InvalidArgumentException( 'SubjectLabel cannot be empty' );
+		}
 	}
 
 }
