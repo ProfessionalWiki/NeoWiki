31c3611a64840709720665e3c6bd66e35b97bb2d
alistair3149 <alistair31494322@gmail.com>
2026-05-14 15:02:02 -0400

    Add ColorType::validate to RedHerb as plugin-contract example

--- tests/RedHerb/src/ColorType.php
+++ tests/RedHerb/src/ColorType.php
@@ -9,11 +9,14 @@ use ProfessionalWiki\NeoWiki\Domain\Schema\PropertyCore;
 use ProfessionalWiki\NeoWiki\Domain\Schema\PropertyDefinition;
 use ProfessionalWiki\NeoWiki\Domain\Validation\Violation;
 use ProfessionalWiki\NeoWiki\Domain\Value\NeoValue;
+use ProfessionalWiki\NeoWiki\Domain\Value\StringValue;
 use ProfessionalWiki\NeoWiki\Domain\Value\ValueType;
 
 class ColorType implements PropertyType {
 
 	public const NAME = 'color';
+
+	private const HEX_COLOR_REGEX = '/^#[0-9a-fA-F]{6}$/';
 
 	public function getTypeName(): string {
 		return self::NAME;
@@ -35,7 +38,45 @@ class ColorType implements PropertyType {
 	 * @return Violation[]
 	 */
 	public function validate( NeoValue $value, PropertyDefinition $definition ): array {
-		return [];
+		if ( !$value instanceof StringValue ) {
+			return [];
+		}
+
+		if ( !$definition instanceof ColorProperty ) {
+			return [];
+		}
+
+		if ( $definition->isRequired() && $value->strings === [] ) {
+			return [ new Violation( propertyName: null, code: 'required' ) ];
+		}
+
+		$allowed = $definition->getAllowedColors();
+		$allowedSet = $allowed === [] ? null : array_flip( $allowed );
+
+		$violations = [];
+
+		foreach ( $value->strings as $index => $part ) {
+			if ( preg_match( self::HEX_COLOR_REGEX, $part ) !== 1 ) {
+				$violations[] = new Violation(
+					propertyName: null,
+					code: 'invalid-color',
+					args: [ $part ],
+					valuePartIndex: $index,
+				);
+				continue;
+			}
+
+			if ( $allowedSet !== null && !isset( $allowedSet[$part] ) ) {
+				$violations[] = new Violation(
+					propertyName: null,
+					code: 'invalid-option',
+					args: [ $part ],
+					valuePartIndex: $index,
+				);
+			}
+		}
+
+		return $violations;
 	}
 
 }
